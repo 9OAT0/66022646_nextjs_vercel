20a45d54ac820f166794a2735d6d4de606f58cfe
9OAT0 <oathaha30@gmail.com>
2024-11-27 02:30:38 +0700

    update about desing that i don't have any idea

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
 export default function HomePage() {
     return (
-        <>
-            <div className="flex justify-center items-center min-h-screen relative">
+    <div className="bg-black text-white pb-10">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="relative flex justify-center items-center">
                 <img
                     src="/S__2940938.jpg"
                     alt="just photo"
                     className="w-[500px] h-[350px] object-cover"
                 />
-                <div className="absolute text-4xl font-bold z-10 text-center text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    THANACHART
+                <div className="absolute text-4xl font-bold z-10 text-center mix-blend-difference top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    THANACHART CHARTMONTREE
                 </div>
             </div>
-        </>
+
+            <div className="text-2xl font-medium text-center mt-10">
+                This my resume
+            </div>
+        </div>
+        <div className="flex justify-center items-center">
+            <div className="text-6xl font-mono">
+                About me. <br />
+                <p className="text-xs">
+                    I'm a software developer i predict my backend-dev experience
+                </p>
+            </div>
+        </div>
+    </div>
     );
 }
